60b648cf0fc5903183010ce5c7d35e0b81e143ea
darshilkantaria11 <91153906+darshilkantaria11@users.noreply.github.com>
2022-10-31 21:52:35 +0530

    Add files via upload

---
+++ src/js/Last.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import last from "../Images/last.png";
+import "../css/Last.css";
+
+export const Last = () => {
+    return (
+        <>
+            <div className="contlast">
+            <img className="last" src={last} alt='' />
+            </div>
+        </>
+    )
+}
+
+export default Last;
